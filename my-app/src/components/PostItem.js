085584625b01e224737130e29321f3f2cab8126d
Fabian Lien <fabian.lien@gmail.com>
2022-05-27 18:59:00 +0000

    add Content component and render content from posts.json, refactor to child component

---
+++ my-app/src/components/PostItem.js
@@ -0,0 +1,19 @@
+import React from "react"
+import css from "./css/PostItem.module.css";
+
+
+function PostItem(props) {
+    return (
+        props.savedPosts.map(post => {
+            const { title, name, image, description } = post
+            return <div key={title} className={css.SearchItem}>
+                <p>{title}</p>
+                <p>{name}</p>
+                <img src={image} alt={description} />
+                <p>{description}</p>
+            </div>
+        })
+    )
+}
+
+export default PostItem
