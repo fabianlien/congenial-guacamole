853fceb194e98a79ddfd27fd94ad75379df2d2df
Fabian Lien <fabian.lien@gmail.com>
2022-05-27 15:53:26 +0000

    add conditional rendering to navbar component and refactor to child component

---
+++ my-app/src/components/NavBarForm.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import css from './css/NavBarForm.module.css'
+import NavBarChild from './NavBarChild'
+
+class NavBarForm extends React.Component {
+    
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         isLoggedIn: true
+      }
+    }
+
+    handleClick= () => {
+        this.setState((prevState) => ({isLoggedIn: !prevState.isLoggedIn}))
+    }
+
+    render() {
+        return (
+            <div className={css.NavBar}>
+                <h1>My Gallery</h1>
+                <NavBarChild isLoggedIn={this.state.isLoggedIn} handleClick={this.handleClick} />
+            </div>
+        )
+    }
+}
+
+export default NavBarForm
